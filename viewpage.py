cbe70efa9f11218dfff823f7e4e1962731ef548b
LuckyTungariya <luckytungariya412@gmail.com>
2024-09-22 14:21:31 +0530

    GUI CODE COMPLETED

--- viewpage.py
+++ viewpage.py
@@ -24,19 +24,36 @@ class ViewPage():
         self.bottom_frame1 = ctk.CTkFrame(self.root,fg_color = "#B8B8B8")
         self.bottom_frame1.pack(fill = "both",expand = True)
 
-        self.side_frame = ctk.CTkFrame(self.bottom_frame1,width = 200,fg_color = "#B8B8B8")
-        self.side_frame.pack(side = "right",fill = "y")
+        #self.side_frame = ctk.CTkFrame(self.bottom_frame1,width = 200,fg_color = "#B8B8B8")
+        #self.side_frame.pack(side = "right",fill = "y")
 
-        self.scrollable_frame = ctk.CTkScrollableFrame(self.bottom_frame1,corner_radius = 10,border_width = 1,border_color = "gray",fg_color = "#ffffff",scrollbar_button_color = "lightgray",scrollbar_button_hover_color = "darkgray",orientation = "vertical")
-        self.scrollable_frame.pack(side = "left",fill = "both",expand = True)
+        self.appointment_frame = ctk.CTkFrame(self.bottom_frame1,corner_radius = 10,border_width = 1,border_color = "gray",fg_color = "#ffffff")
+        self.appointment_frame.pack(fill = "both",expand = True,padx = 30,pady = (0,20))
+
+        self.show_appointment_label = ctk.CTkLabel(self.appointment_frame,text = "Appointments",text_color = "#000000",font = ctk.CTkFont(family = "Fantasy",size = 25,weight = "bold"))
+        self.show_appointment_label.pack(anchor = "nw",padx = 10,pady = 10)
+
+        self.scrollable_frame = ctk.CTkScrollableFrame(self.appointment_frame,corner_radius = 15,border_width = 1,border_color = "darkgray",fg_color = "#ffffff",scrollbar_button_color = "lightgray",scrollbar_button_hover_color = "darkgray",orientation = "vertical")
+        self.scrollable_frame.pack(anchor = "nw",fill = "both",expand = True,padx = 30,pady = (5,30))
+
+        self.queue_no_label = ctk.CTkLabel(self.scrollable_frame,text = "QUEUE NO",text_color = "#000000",font = ctk.CTkFont(family = "Luminari",size = 20,weight = "bold"))
+        self.queue_no_label.grid(row = 0,column = 0,sticky = "nw",padx = 10,pady = 10)
+
+        self.patient_name = ctk.CTkLabel(self.scrollable_frame,text = "PATIENT NAME",text_color = "#000000",font = ctk.CTkFont(family = "Luminari",size = 20,weight = "bold"))
+        self.patient_name.grid(row = 0,column = 1,padx = 130,pady = 10)
+
+        self.doctor_name = ctk.CTkLabel(self.scrollable_frame,text = "DOCTOR NAME",text_color = "#000000",font = ctk.CTkFont(family = "Luminari",size = 20,weight = "bold"))
+        self.doctor_name.grid(row = 0,column = 2,padx = 100,pady = 10)
+
+        self.status = ctk.CTkLabel(self.scrollable_frame,text = "STATUS",text_color = "#000000",font = ctk.CTkFont(family = "Luminari",size = 20,weight = "bold"))
+        self.status.grid(row = 0,column = 3,padx = 100,pady = 10)
 
     def back_command(self):
         self.root.destroy()
 
-    def createslots(self,data):
-        self.appointment_label = ctk.CTkLabel(self.scrollable_frame,height = 100,text = data,corner_radius = 15,fg_color = "#C8C8C8",text_color = "#000000",font = ctk.CTkFont(family = "Menlo",size = 20,weight = "bold"))
-        self.appointment_label.pack(side = "top",fill = "x",padx = 10,pady = 10)
-
+    def createslots(self,queue_no,patient_nm,doctor_nm):
+        self.label_slot = ctk.CTkLabel(self.scrollable_frame,height = 35,corner_radius = 15,fg_color = "lightgray")
+        self.label_slot.grid(row = 0,column = 1,fill = "x",padx = 20,pady = 10)
         
 viewpage_object = ViewPage()
 viewpage_object.root.mainloop()
